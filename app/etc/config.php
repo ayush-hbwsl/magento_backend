fe66c17678bb2974877b882871c47516f0a3afa0
ayush-hbwsl <ayush.verma@hbwsl.com>
2024-06-27 17:46:08 +0530

    lecture20

--- app/etc/config.php
+++ app/etc/config.php
@@ -377,6 +377,7 @@ return [
         'PayPal_BraintreeGiftCardAccount' => 1,
         'PayPal_BraintreeGiftWrapping' => 1,
         'PayPal_BraintreeGraphQl' => 1,
+        'Training_Contact' => 1,
         'Training_Example' => 1,
         'Training_HeadCssJs' => 1,
         'Training_HeaderFooterScripts' => 1,
